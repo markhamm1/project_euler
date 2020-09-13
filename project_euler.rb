e29ad2dd865f36dff9346c11c90e30270ae1abcf
Mark <markandrewhamm@gmail.com>
2020-09-13 15:07:41 -0500

    project euler - problem 5

--- project_euler.rb
+++ project_euler.rb
@@ -1,38 +1,25 @@
-def sum_of_threes_and_fives(max_number)
-  sum = 0
-  index = 0
-  max_number.times do
-    if index % 3 == 0
-      sum += index
-    elsif index % 5 == 0
-      sum += index
+# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+def smallest_without_remainder(max_number)
+  lowest_number = max_number
+  while true
+    i = 1
+    (max_number - 1).times do
+      if lowest_number % i == 0
+        i += 1
+      else
+        break
+      end
     end
-    index += 1
+    if i == max_number
+      break
+    else
+      lowest_number += max_number
+    end
   end
-  p sum
+  lowest_number
 end
 
-
-
-# def sum_of_fives(max_number)
-#   sum = 0
-#   index = 0
-#   max_number.times do
-#     if index % 5 == 0
-#       sum += index
-#     end
-#     index += 1
-#   end
-#   sum
-# end
-
-# def print_sum(threes, fives)
-#   p threes + fives
-# end
-
-max_number = 1000
-sum_of_threes_and_fives(max_number)
-
-# threes = sum_of_threes(max_number)
-# fives = sum_of_fives(max_number)
-# print_sum(threes, fives)
+max_number = 20
+p smallest_without_remainder(max_number)
